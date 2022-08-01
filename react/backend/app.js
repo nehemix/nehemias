8f3fb3ed2b8864bccf5e9ed97efc4837f3b32a24
nehemix <nehemiasaquino@gmail.com>
2022-08-01 02:43:04 -0300

    login

--- react/backend/app.js
+++ react/backend/app.js
@@ -5,11 +5,12 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 require('dotenv').config();
-
+var session = require('express-session');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/admin/login');
-var adminRouter = require('./routes/admin/precios')
+var adminRouter = require('./routes/admin/precios');
+const { nextTick } = require('process');
 
 var app = express();
 
@@ -23,11 +24,31 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.use(session({
+  secret: 'ljA3ek9CkD84',
+  cookie: { maxAge: null},
+  resave: false,
+  saveUninitialized: true
+}))
+
+secured = async (req, res, next) => {
+  try{
+    console.log(req.session.id_usuario);
+    if (req.session.id_usuario){
+      next()
+    } else{
+      res.redirect('/admin/login');
+    }
+  } catch (error){
+    console.log(error);
+  }
+}
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/admin/login', loginRouter); //login.js
 app.use('/admin/precios', adminRouter);
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- react/backend/routes/admin/precios.js
+++ react/backend/routes/admin/precios.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var usuariosModel = require('./../../models/usuariosModel')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
